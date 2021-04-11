07b38c339fde23e456eaa32c5dce8593216bad3e
Kieren Pearson <kierenpearson9@gmail.com>
2021-04-11 11:28:39 +1000

    Bench: 18180221

--- Halogen/src/Search.h
+++ Halogen/src/Search.h
@@ -9,27 +9,27 @@
 
 /*Tuneable search constants*/
 
-inline double LMR_constant = -1.48;
-inline double LMR_coeff = 1.10;
+inline double LMR_constant = -2.16;
+inline double LMR_coeff = 1.18;
 
 inline int Null_constant = 5;
-inline int Null_depth_quotent = 6;
-inline int Null_beta_quotent = 277;
+inline int Null_depth_quotent = 7;
+inline int Null_beta_quotent = 197;
 
-inline int Futility_constant = 75;
-inline int Futility_coeff = 46;
-inline int Futility_depth = 10;
+inline int Futility_constant = 79;
+inline int Futility_coeff = 54;
+inline int Futility_depth = 9;
 
-inline int Aspiration_window = 18;
+inline int Aspiration_window = 16;
 
-inline int Delta_margin = 235;
+inline int Delta_margin = 220;
 
-inline int SNMP_coeff = 109;
-inline int SNMP_depth = 7;
+inline int SNMP_coeff = 77;
+inline int SNMP_depth = 6;
 
 inline int LMP_constant = 8;
-inline int LMP_coeff = 8;
-inline int LMP_depth = 6;
+inline int LMP_coeff = 6;
+inline int LMP_depth = 7;
 
 /*----------------*/
 
