17540f0ddcef235349710bfb560d67ab819f4a6e
Kieren Pearson <kierenpearson9@gmail.com>
2021-05-04 20:44:42 +1000

    Remove the network to remove any chance we contaminate data

--- Halogen/src/Network.cpp
+++ Halogen/src/Network.cpp
@@ -1,5 +1,5 @@
 #include "Network.h"
-#include "epoch450.net"
+#include "epochXXX.net"
 
 std::array<std::array<int16_t, HIDDEN_NEURONS>, INPUT_NEURONS> Network::hiddenWeights = {};
 std::array<int16_t, HIDDEN_NEURONS> Network::hiddenBias = {};
