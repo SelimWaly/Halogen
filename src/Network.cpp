17c1fc668aef66eccb432c93a996b165128a20d7
Kieren Pearson <kierenpearson9@gmail.com>
2021-06-23 18:23:17 +1000

    Bench: 29027530

--- src/Network.cpp
+++ src/Network.cpp
@@ -11,7 +11,7 @@ int16_t Network::outputBias = {};
 constexpr int16_t MAX_VALUE = 128;
 constexpr int16_t PRECISION = ((size_t)std::numeric_limits<int16_t>::max() + 1) / MAX_VALUE;
 constexpr int32_t SQUARE_PRECISION = (int32_t)PRECISION * PRECISION;
-constexpr double SCALE_FACTOR = 0.94;   //Found empirically to maximize elo
+constexpr double SCALE_FACTOR = 0.99;   //Found empirically to maximize elo
 
 template<typename T, size_t SIZE>
 [[nodiscard]] std::array<T, SIZE> ReLU(const std::array<T, SIZE>& source)
