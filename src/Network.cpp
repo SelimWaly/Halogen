4c8cc1443ea992b2a10fe1e9f54afe14d99ecd7e
Kieren Pearson <kierenpearson9@gmail.com>
2021-11-07 17:15:42 +1100

    Bench: 59547797

--- src/Network.cpp
+++ src/Network.cpp
@@ -2,7 +2,7 @@
 #include "Position.h"
 #include "incbin/incbin.h"
 
-INCBIN(Net, "epoch40.nn");
+INCBIN(Net, "epoch30.nn");
 
 std::array<std::array<int16_t, HIDDEN_NEURONS>, INPUT_NEURONS> Network::hiddenWeights = {};
 std::array<int16_t, HIDDEN_NEURONS> Network::hiddenBias = {};
