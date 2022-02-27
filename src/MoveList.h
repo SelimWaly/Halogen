bcd9d15a6053d10dd87162990d6a8960e7b3df8f
KierenP <kierenpearson9@gmail.com>
2022-02-27 21:18:30 +1100

    Bench: 28974643

--- src/MoveList.h
+++ src/MoveList.h
@@ -4,6 +4,7 @@
 #include <cstddef>
 #include <cstdint>
 #include <iterator>
+#include <memory>
 #include <type_traits>
 
 #include "BitBoardDefine.h"
@@ -37,37 +38,35 @@ struct ExtendedMove
     int16_t SEE;
 };
 
-template <typename T>
-class FixedVector;
-
-using ExtendedMoveList = FixedVector<ExtendedMove>;
-using BasicMoveList = FixedVector<Move>;
-
 // Internally, a FixedVector is an array
 // You can Append() moves to the list and it will internally keep track of how many moves have been added
 // You can get iterators at the Begin() and End() of the move list
 
-template <typename T>
+template <typename T, size_t MAX_SIZE>
 class FixedVector
 {
 private:
-    std::array<T, 256> list;
+    std::array<T, MAX_SIZE> list;
     static_assert(std::is_trivial_v<T>);
 
 public:
     using iterator = typename decltype(list)::iterator;
     using const_iterator = typename decltype(list)::const_iterator;
 
-    iterator begin();
-    iterator end();
+    iterator begin() { return list.begin(); }
+    iterator end() { return list.begin() + moveCount; }
 
-    const_iterator begin() const;
-    const_iterator end() const;
+    const_iterator begin() const { return list.begin(); }
+    const_iterator end() const { return list.begin() + moveCount; }
 
-    size_t size() const;
+    size_t size() const { return moveCount; }
 
-    void clear();
-    void erase(size_t index);
+    void clear() { moveCount = 0; }
+    void erase(size_t index)
+    {
+        std::move(list.begin() + index + 1, list.end(), list.begin() + index);
+        moveCount--;
+    }
 
     const T& operator[](size_t index) const { return list[index]; }
     T& operator[](size_t index) { return list[index]; }
@@ -75,7 +74,7 @@ public:
     template <typename... Args>
     void emplace_back(Args&&... args)
     {
-        list[moveCount++] = { args... };
+        list[moveCount++] = T(std::forward<Args>(args)...);
     }
 
     template <class InputIt>
@@ -88,3 +87,6 @@ public:
 private:
     size_t moveCount = 0;
 };
+
+using ExtendedMoveList = FixedVector<ExtendedMove, 256>;
+using BasicMoveList = FixedVector<Move, 256>;
