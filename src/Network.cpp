d9cb9e47dfd0135667d766eaa509999889865cec
KierenP <kierenpearson9@gmail.com>
2022-03-01 22:44:55 +1100

    Bench: 96097010

--- src/Network.cpp
+++ src/Network.cpp
@@ -9,7 +9,7 @@
 #include "Position.h"
 #include "incbin/incbin.h"
 
-INCBIN(Net, "768-1_g113057.nn");
+INCBIN(Net, "768-1_g227822.nn");
 
 std::array<float, INPUT_NEURONS> Network::l1_weight = {};
 float Network::l1_bias = {};
