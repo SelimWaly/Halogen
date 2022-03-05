b9da4f81caf84cd495692fef94d6edac61d7a74c
KierenP <kierenpearson9@gmail.com>
2022-03-06 00:57:27 +1100

    Train networks with a hidden layer

--- src/td-leaf/TrainableNetwork.h
+++ src/td-leaf/TrainableNetwork.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "HalogenNetwork.h"
+#include <array>
 #include <mutex>
 
 class TrainableNetwork : public HalogenNetwork
@@ -13,17 +14,20 @@ public:
     void Backpropagate(double loss_gradient, const std::vector<int>& sparse_inputs);
     void PrintNetworkDiagnostics() const;
 
+    struct adam_state
+    {
+        float m;
+        float v;
+
+        constexpr static double beta_1 = 0.9;
+        constexpr static double beta_2 = 0.999;
+        constexpr static double alpha = 0.01;
+        constexpr static double epsilon = 10e-8;
+    };
+
 private:
-    mutable std::mutex mutex;
+    static std::recursive_mutex mutex;
 
-    constexpr static double beta_1 = 0.9;
-    constexpr static double beta_2 = 0.999;
-    constexpr static double alpha = 0.1;
-    constexpr static double epsilon = 10e-8;
-
-    static std::array<std::array<float, architecture[1]>, architecture[0]> l1_weight_m;
-    static std::array<float, architecture[1]> l1_bias_m;
-
-    static std::array<std::array<float, architecture[1]>, architecture[0]> l1_weight_v;
-    static std::array<float, architecture[1]> l1_bias_v;
+    static TransposeLayer<adam_state, architecture[0], architecture[1]> l1_adam;
+    static Layer<adam_state, architecture[1], architecture[2]> l2_adam;
 };
