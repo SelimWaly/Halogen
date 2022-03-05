69abfbe5f87b1fdaaefba2520b6dd08d16c7a524
KierenP <kierenpearson9@gmail.com>
2022-03-06 00:57:27 +1100

    Train networks with a hidden layer

--- src/td-leaf/HalogenNetwork.cpp
+++ src/td-leaf/HalogenNetwork.cpp
@@ -4,12 +4,12 @@
 
 #include "matrix_operations.h"
 
-std::array<std::array<float, architecture[1]>, architecture[0]> HalogenNetwork::l1_weight;
-std::array<float, architecture[1]> HalogenNetwork::l1_bias;
+TransposeLayer<float, architecture[0], architecture[1]> HalogenNetwork::l1;
+Layer<float, architecture[1], architecture[2]> HalogenNetwork::l2;
 
 void HalogenNetwork::Recalculate(const Position& position)
 {
-    AccumulatorStack = { { l1_bias } };
+    AccumulatorStack = { { l1.bias } };
 
     for (int i = 0; i < N_PIECES; i++)
     {
@@ -40,7 +40,7 @@ void HalogenNetwork::AddInput(Square square, Pieces piece)
 
     for (size_t j = 0; j < architecture[1]; j++)
     {
-        AccumulatorStack.back()[j] += l1_weight[input][j];
+        AccumulatorStack.back()[j] += l1.weight[input][j];
     }
 }
 
@@ -50,13 +50,13 @@ void HalogenNetwork::RemoveInput(Square square, Pieces piece)
 
     for (size_t j = 0; j < architecture[1]; j++)
     {
-        AccumulatorStack.back()[j] -= l1_weight[input][j];
+        AccumulatorStack.back()[j] -= l1.weight[input][j];
     }
 }
 
 int16_t HalogenNetwork::Eval(Players) const
 {
-    return AccumulatorStack.back()[0];
+    return l2.bias[0] + dot_product<float>(copy_ReLU(AccumulatorStack.back()), l2.weight[0]);
 }
 
 int HalogenNetwork::index(Square square, Pieces piece) const
--- src/td-leaf/TrainableNetwork.cpp
+++ src/td-leaf/TrainableNetwork.cpp
@@ -1,16 +1,37 @@
 #include "TrainableNetwork.h"
 #include "../Position.h"
+#include "HalogenNetwork.h"
+#include "matrix_operations.h"
+#include <cassert>
 #include <cmath>
 #include <fstream>
+#include <functional>
 #include <iostream>
+#include <memory>
 #include <mutex>
 #include <random>
+#include <tuple>
+#include <type_traits>
+#include <utility>
 
-std::array<std::array<float, architecture[1]>, architecture[0]> TrainableNetwork::l1_weight_m;
-std::array<float, architecture[1]> TrainableNetwork::l1_bias_m;
+// apply the adam gradients to the weight layer
+template <template <typename, size_t, size_t> class layer_t, typename T, size_t in, size_t out>
+void apply_gradient(layer_t<T, in, out>& layer, const layer_t<TrainableNetwork::adam_state, in, out>& adam);
 
-std::array<std::array<float, architecture[1]>, architecture[0]> TrainableNetwork::l1_weight_v;
-std::array<float, architecture[1]> TrainableNetwork::l1_bias_v;
+// given a loss gradient of layer l, and an activation for layer l-1, update the adam states in layer l
+template <typename T, size_t in, size_t out>
+void update_gradient(Layer<TrainableNetwork::adam_state, in, out>& adam, const std::array<T, out>& gradient, const std::array<T, in>& activation);
+
+template <typename T, size_t in, size_t out>
+void update_gradient_sparse(TransposeLayer<TrainableNetwork::adam_state, in, out>& adam, const std::array<T, out>& gradient, const std::vector<int>& activation);
+
+template <typename T, size_t in, size_t out, typename Activation_prime>
+std::array<T, in> calculate_loss_gradient(Layer<T, in, out>& layer, const std::array<T, out>& gradient, const std::array<T, in>& activation, Activation_prime&& activation_prime);
+
+TransposeLayer<TrainableNetwork::adam_state, architecture[0], architecture[1]> TrainableNetwork::l1_adam;
+Layer<TrainableNetwork::adam_state, architecture[1], architecture[2]> TrainableNetwork::l2_adam;
+
+std::recursive_mutex TrainableNetwork::mutex;
 
 std::vector<int> TrainableNetwork::GetSparseInputs(const Position& position) const
 {
@@ -36,79 +57,212 @@ std::vector<int> TrainableNetwork::GetSparseInputs(const Position& position) con
 
 void TrainableNetwork::InitializeWeightsRandomly() const
 {
-    std::lock_guard<std::mutex> lock(mutex);
+    std::lock_guard<std::recursive_mutex> lock(mutex);
 
     std::mt19937 gen(0);
-    std::normal_distribution<float> dis(0, 0);
 
-    for (auto& row : l1_weight)
+    auto initialize = [&gen](auto&& layer)
     {
-        for (auto& weight : row)
+        // Kaiming He initialization
+        std::normal_distribution<float> dis(0, sqrt(2.0 / layer.inputs));
+
+        for (auto& row : layer.weight)
         {
-            weight = dis(gen);
+            for (auto& element : row)
+            {
+                element = dis(gen);
+            }
         }
-    }
+    };
 
-    for (auto& bias : l1_bias)
-    {
-        bias = dis(gen);
-    }
+    initialize(l1);
+    initialize(l2);
+
+    PrintNetworkDiagnostics();
 }
 
 void TrainableNetwork::SaveWeights(const std::string& filename) const
 {
-    std::lock_guard<std::mutex> lock(mutex);
+    std::lock_guard<std::recursive_mutex> lock(mutex);
 
     std::ofstream file(filename, std::ios::out | std::ios::binary);
 
-    file.write(reinterpret_cast<const char*>(l1_weight.data()), sizeof(l1_weight));
-    file.write(reinterpret_cast<const char*>(l1_bias.data()), sizeof(l1_bias));
+    auto save_bias = [&file](auto& layer)
+    {
+        for (size_t i = 0; i < layer.bias.size(); i++)
+        {
+            file.write(reinterpret_cast<const char*>(&layer.bias[i]), sizeof(float));
+        }
+    };
+
+    auto save_layer = [&file, &save_bias](auto& layer)
+    {
+        for (size_t i = 0; i < layer.weight.size(); i++)
+        {
+            for (size_t j = 0; j < layer.weight[i].size(); j++)
+            {
+                file.write(reinterpret_cast<const char*>(&layer.weight[i][j]), sizeof(float));
+            }
+        }
+
+        save_bias(layer);
+    };
+
+    auto save_transpose_layer = [&file, &save_bias](auto& layer)
+    {
+        for (size_t i = 0; i < layer.weight.size(); i++)
+        {
+            for (size_t j = 0; j < layer.weight[i].size(); j++)
+            {
+                file.write(reinterpret_cast<const char*>(&layer.weight[j][i]), sizeof(float));
+            }
+        }
+
+        save_bias(layer);
+    };
+
+    save_transpose_layer(l1);
+    save_layer(l2);
 }
 
 void TrainableNetwork::Backpropagate(double loss_gradient, const std::vector<int>& sparse_inputs)
 {
-    std::lock_guard<std::mutex> lock(mutex);
+    std::lock_guard<std::recursive_mutex> lock(mutex);
 
-    for (size_t i = 0; i < l1_weight.size(); i++)
+    // do the forward pass and save the activations:
+
+    std::array<float, architecture[1]> l1_activation = {};
+
+    for (size_t i = 0; i < sparse_inputs.size(); i++)
     {
-        double g = std::find(sparse_inputs.begin(), sparse_inputs.end(), i) != sparse_inputs.end() ? loss_gradient : 0;
-
-        l1_weight_m[i][0] = beta_1 * l1_weight_m[i][0] + (1 - beta_1) * g;
-        l1_weight_v[i][0] = beta_2 * l1_weight_v[i][0] + (1 - beta_2) * g * g;
-
-        l1_weight[i][0] += -alpha * l1_weight_m[i][0] / std::sqrt(l1_weight_v[i][0] + epsilon);
+        for (int j = 0; j < architecture[1]; j++)
+        {
+            l1_activation[j] += l1.weight[sparse_inputs[i]][j];
+        }
     }
-    l1_bias_m[0] = beta_1 * l1_bias_m[0] + (1 - beta_1) * loss_gradient;
-    l1_bias_v[0] = beta_2 * l1_bias_v[0] + (1 - beta_2) * loss_gradient * loss_gradient;
 
-    l1_bias[0] += -alpha * l1_bias_m[0] / std::sqrt(l1_bias_v[0] + epsilon);
+    apply_ReLU(l1_activation);
+
+    // get loss gradients
+    auto l2_loss_gradient = std::array { static_cast<float>(loss_gradient) };
+    auto l1_loss_gradient = calculate_loss_gradient(l2, l2_loss_gradient, l1_activation, [](float x)
+        { return x > 0.0f; });
+
+    // gradient updates
+    update_gradient_sparse(l1_adam, l1_loss_gradient, sparse_inputs);
+    update_gradient(l2_adam, l2_loss_gradient, l1_activation);
+
+    // now actually apply the gradients
+    apply_gradient(l1, l1_adam);
+    apply_gradient(l2, l2_adam);
+}
+
+template <template <typename, size_t, size_t> class layer_t, typename T, size_t in, size_t out>
+void apply_gradient(layer_t<T, in, out>& layer, const layer_t<TrainableNetwork::adam_state, in, out>& adam)
+{
+    for (size_t i = 0; i < layer.weight.size(); i++)
+    {
+        for (size_t j = 0; j < layer.weight[i].size(); j++)
+        {
+            layer.weight[i][j] += -TrainableNetwork::adam_state::alpha * adam.weight[i][j].m / std::sqrt(adam.weight[i][j].v + TrainableNetwork::adam_state::epsilon);
+        }
+    }
+
+    for (size_t i = 0; i < layer.bias.size(); i++)
+    {
+        layer.bias[i] += -TrainableNetwork::adam_state::alpha * adam.bias[i].m / std::sqrt(adam.bias[i].v + TrainableNetwork::adam_state::epsilon);
+    }
+}
+
+template <typename T, size_t in, size_t out>
+void update_gradient(Layer<TrainableNetwork::adam_state, in, out>& adam, const std::array<T, out>& gradient, const std::array<T, in>& activation)
+{
+    for (size_t i = 0; i < adam.weight.size(); i++)
+    {
+        for (size_t j = 0; j < adam.weight[i].size(); j++)
+        {
+            double g = gradient[i] * activation[j];
+            adam.weight[i][j].m = TrainableNetwork::adam_state::beta_1 * adam.weight[i][j].m + (1 - TrainableNetwork::adam_state::beta_1) * g;
+            adam.weight[i][j].v = TrainableNetwork::adam_state::beta_2 * adam.weight[i][j].v + (1 - TrainableNetwork::adam_state::beta_2) * g * g;
+        }
+
+        double g = gradient[i];
+        adam.bias[i].m = TrainableNetwork::adam_state::beta_1 * adam.bias[i].m + (1 - TrainableNetwork::adam_state::beta_1) * g;
+        adam.bias[i].v = TrainableNetwork::adam_state::beta_2 * adam.bias[i].v + (1 - TrainableNetwork::adam_state::beta_2) * g * g;
+    }
+}
+
+template <typename T, size_t in, size_t out>
+void update_gradient_sparse(TransposeLayer<TrainableNetwork::adam_state, in, out>& adam, const std::array<T, out>& gradient, const std::vector<int>& sparse_inputs)
+{
+    for (size_t i = 0; i < adam.weight.size(); i++)
+    {
+        double activation = std::find(sparse_inputs.begin(), sparse_inputs.end(), i) != sparse_inputs.end() ? 1 : 0;
+
+        for (size_t j = 0; j < adam.weight[i].size(); j++)
+        {
+            double g = gradient[j] * activation;
+            adam.weight[i][j].m = TrainableNetwork::adam_state::beta_1 * adam.weight[i][j].m + (1 - TrainableNetwork::adam_state::beta_1) * g;
+            adam.weight[i][j].v = TrainableNetwork::adam_state::beta_2 * adam.weight[i][j].v + (1 - TrainableNetwork::adam_state::beta_2) * g * g;
+        }
+    }
+
+    for (size_t i = 0; i < adam.bias.size(); i++)
+    {
+        double g = gradient[i];
+        adam.bias[i].m = TrainableNetwork::adam_state::beta_1 * adam.bias[i].m + (1 - TrainableNetwork::adam_state::beta_1) * g;
+        adam.bias[i].v = TrainableNetwork::adam_state::beta_2 * adam.bias[i].v + (1 - TrainableNetwork::adam_state::beta_2) * g * g;
+    }
+}
+
+template <typename T, size_t in, size_t out, typename Activation_prime>
+std::array<T, in> calculate_loss_gradient(Layer<T, in, out>& layer, const std::array<T, out>& gradient, const std::array<T, in>& activation, Activation_prime&& activation_prime)
+{
+    std::array<T, in> loss_gradient = {};
+
+    for (size_t i = 0; i < layer.weight.size(); i++)
+    {
+        for (size_t j = 0; j < layer.weight[i].size(); j++)
+        {
+            loss_gradient[j] += gradient[i] * layer.weight[i][j];
+        }
+    }
+
+    for (size_t i = 0; i < loss_gradient.size(); i++)
+    {
+        loss_gradient[i] *= activation_prime(activation[i]);
+    }
+
+    return loss_gradient;
 }
 
 void TrainableNetwork::PrintNetworkDiagnostics() const
 {
-    std::lock_guard<std::mutex> lock(mutex);
+    std::lock_guard<std::recursive_mutex> lock(mutex);
 
-    for (int i = 0; i < N_PIECES; i++)
+    auto print_layer = [&](auto&& layer)
     {
-        float sum = 0;
-
-        for (int j = 0; j < N_SQUARES; j++)
+        for (size_t i = 0; i < layer.weight.size(); i++)
         {
-            sum += l1_weight[i * 64 + j][0];
-
-            std::cout << l1_weight[i * 64 + j][0] << " ";
-
-            if (j % N_FILES == N_FILES - 1)
+            for (size_t j = 0; j < layer.weight[i].size(); j++)
             {
-                std::cout << std::endl;
+                std::cout << layer.weight[i][j] << " ";
             }
+
+            std::cout << std::endl;
         }
 
-        sum /= N_SQUARES;
-
-        std::cout << "piece " << i << ": " << sum << std::endl;
         std::cout << std::endl;
-    }
 
-    std::cout << "bias: " << l1_bias[0] << std::endl;
+        for (size_t i = 0; i < layer.bias.size(); i++)
+        {
+            std::cout << layer.bias[i] << " ";
+        }
+
+        std::cout << std::endl;
+        std::cout << std::endl;
+    };
+
+    print_layer(l1);
+    print_layer(l2);
 }
