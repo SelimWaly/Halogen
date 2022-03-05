0df6f3e0acdea09d56944b9b070db5c7ec58db20
KierenP <kierenpearson9@gmail.com>
2022-03-06 00:58:49 +1100

    Bench: 177859641

--- src/Network.h
+++ src/Network.h
@@ -7,13 +7,13 @@
 #include "BitBoardDefine.h"
 
 constexpr size_t INPUT_NEURONS = 12 * 64;
-constexpr size_t HIDDEN_NEURONS = 512;
+constexpr size_t HIDDEN_NEURONS = 8;
 
 class Position;
 
 struct HalfAccumulator
 {
-    std::array<std::array<int16_t, HIDDEN_NEURONS>, N_PLAYERS> side;
+    std::array<float, HIDDEN_NEURONS> vals;
 };
 
 class Network
@@ -38,8 +38,8 @@ public:
 private:
     std::vector<HalfAccumulator> AccumulatorStack;
 
-    static std::array<std::array<int16_t, HIDDEN_NEURONS>, INPUT_NEURONS> hiddenWeights;
-    static std::array<int16_t, HIDDEN_NEURONS> hiddenBias;
-    static std::array<int16_t, HIDDEN_NEURONS * 2> outputWeights;
-    static int16_t outputBias;
+    static std::array<std::array<float, HIDDEN_NEURONS>, INPUT_NEURONS> hiddenWeights;
+    static std::array<float, HIDDEN_NEURONS> hiddenBias;
+    static std::array<float, HIDDEN_NEURONS> outputWeights;
+    static float outputBias;
 };
